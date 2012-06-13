c456bcf716092080e22b15fd44366d7448aa6294
nayan kalita <nayan.kalita@inmobi.com>
2012-06-13 22:24:28 +0530

    prints message

---
+++ cookbooks/log_hello/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: log_hello
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+log "my name is nayan and its my first recipe!!!"
+
